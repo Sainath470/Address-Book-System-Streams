13717d80a254fb7141f3b6753f0108b462b41f22
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-08 23:26:30 +0530

    ADDED : Ability to add contacts to address book

---
+++ src/main/java/AddressBookRunner.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class AddressBookRunner {
+    private static Scanner scannerRunner = new Scanner(System.in);
+    private static Map<String, AddressBook> addressBookMap = new HashMap<>();
+
+
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME");
+        boolean isExit = false;
+        while (!isExit) {
+            System.out.println("Press 1 ADD ADDRESS BOOK\nPRESS 2 FOR ADDING PERSON\nPRESS 3 FOR EXIT");
+            int choice = scannerRunner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the Address Book Name");
+                    addressBookMap.put(scannerRunner.next(), new AddressBook());
+                    System.out.println(addressBookMap.toString());
+                    break;
+                case 2:
+                    System.out.println("Enter address book name to add person");
+                    String addressBookName = scannerRunner.next();
+                    if (addressBookMap.containsKey(addressBookName)) {
+                        AddressBook bookSystem = addressBookMap.get(addressBookName);
+                        bookSystem.createPerson();
+                    } else {
+                        System.out.println("Address Book is not present");
+                    }
+                    break;
+
+                default:
+                    isExit = true;
+                    break;
+                }
+         }
+    }
+}
+
+
+
---
+++ src/main/java/Person.java
@@ -0,0 +1,112 @@
+import java.util.Objects;
+public class Person {
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String phoneNumber;
+        private String emailId;
+        private int zipCode;
+
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public int getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(int zipCode) {
+            this.zipCode = zipCode;
+        }
+
+        public String getEmailId() {
+            return emailId;
+        }
+
+        public void setEmailId(String emailId) {
+            this.emailId = emailId;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", address='" + address + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    ", zip=" + zipCode +
+                    ", email='" + emailId + '\'' +
+                    ", phoneNumber='" + phoneNumber + '\'' +
+                    '}';
+
+        }
+
+    
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o){
+            return true;
+        }
+        if(o==null || getClass() != o.getClass()){
+            return false;
+        }
+        Person person =(Person) o;
+        return Objects.equals(firstName, person.firstName)
+                && Objects.equals(lastName, person.lastName);
+    }
+
+    public int hashCode(){
+        return Objects.hash(firstName, lastName);
+    }
+
+}
